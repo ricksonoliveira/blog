56cdb9f8116c6da3c03c10543edc8db0dce9a1a2
ricksonolveira <ricksonoliveira>
2022-01-14 19:21:02 -0300

    adding posts indeX page

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -2,6 +2,18 @@ defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
   def index(conn, params) do
+    posts = [
+      %{
+        title: "Phoenix"
+      },
+      %{
+        title: "Liveview"
+      },
+      %{
+        title: "Postgres"
+      }
+    ]
 
+    render(conn, "index.html", posts: posts)
   end
 end
