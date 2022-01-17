f0b125e507b4425dcb50be036d9e1ed8d3a18a7d
ricksonolveira <ricksonoliveira>
2022-01-17 16:49:26 -0300

    feat: adding show posts endpoint

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -4,16 +4,23 @@ defmodule BlogWeb.PostController do
   def index(conn, params) do
     posts = [
       %{
+        id: 1,
         title: "Phoenix"
       },
       %{
+        id: 2,
         title: "Liveview"
       },
       %{
+        id: 3,
         title: "Postgres"
       }
     ]
 
     render(conn, "index.html", posts: posts)
+  end
+
+  def show(conn, params) do
+    render(conn, "show.html")
   end
 end
