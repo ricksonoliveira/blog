50d83e253b7073ab4d4533c6ede305df7175dc30
ricksonolveira <ricksonoliveira>
2022-01-21 14:51:26 -0300

    feat:  creating form to add posts

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -1,26 +1,20 @@
 defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
-  def index(conn, params) do
-    posts = [
-      %{
-        id: 1,
-        title: "Phoenix"
-      },
-      %{
-        id: 2,
-        title: "Liveview"
-      },
-      %{
-        id: 3,
-        title: "Postgres"
-      }
-    ]
+  alias Blog.Posts.Post
 
+  def index(conn, _params) do
+    posts = Blog.Repo.all(Post)
     render(conn, "index.html", posts: posts)
   end
 
-  def show(conn, params) do
-    render(conn, "show.html")
+  def show(conn, %{"id" => id}) do
+    post = Blog.Repo.get!(Post, id)
+    render(conn, "show.html", post: post)
+  end
+
+  def new(conn, _params) do
+    changeset = Post.changeset(%Post{})
+    render(conn, "new.html", changeset: changeset)
   end
 end
