1a1d08c929765bd0e3b77a46264dc7fbfa51afd6
ricksonolveira <ricksonoliveira>
2022-01-27 17:33:20 -0300

    feat: comments comming to websocket

--- lib/blog_web/channels/comments_channel.ex
+++ lib/blog_web/channels/comments_channel.ex
@@ -1,7 +1,10 @@
 defmodule BlogWeb.CommentsChannel do
   use BlogWeb, :channel
 
-  def join(name, _payload, socket), do: {:ok, %{name: "It's working!"}, socket}
+  def join("comments:" <> post_id, payload, socket) do
+    post = Blog.Posts.get_post_with_comments!(post_id)
+    {:ok, %{comments: post.comments}, socket}
+  end
 
   def handle_id() do
 
