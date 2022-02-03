aa92c8d94ad8811836d1e0d8ef11369ddb29824f
ricksonolveira <ricksonoliveira>
2022-02-03 12:47:32 -0300

    feat: creating user

--- lib/blog_web/controllers/auth_controller.ex
+++ lib/blog_web/controllers/auth_controller.ex
@@ -1,11 +1,19 @@
-defmodule AuthController do
+defmodule BlogWeb.AuthController do
   use BlogWeb, :controller
 
-  def request(conn, _params) do
-    render(conn, "index.html")
-  end
+  plug Ueberauth
 
-  def callback(conn, _params) do
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do
+    user = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      image: auth.info.image,
+      provider: provider
+    }
+
+    IO.inspect(user)
     render(conn, "index.html")
   end
 end
