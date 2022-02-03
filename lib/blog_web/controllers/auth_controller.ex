87abdb20936f692e92b815b536119db476ee3577
ricksonolveira <ricksonoliveira>
2022-02-03 12:26:31 -0300

    feat: ueberauth controller

---
+++ lib/blog_web/controllers/auth_controller.ex
@@ -0,0 +1,11 @@
+defmodule AuthController do
+  use BlogWeb, :controller
+
+  def request(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def callback(conn, _params) do
+    render(conn, "index.html")
+  end
+end
