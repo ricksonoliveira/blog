739b90ac669ffb8fb18becd579721c21e014b2a3
ricksonolveira <ricksonoliveira>
2022-02-04 17:05:14 -0300

    feat: create user relation with posts

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -25,7 +25,7 @@ defmodule BlogWeb.PostController do
   end
 
   def create(conn, %{"post" => post}) do
-    case Posts.create_post(post) do
+    case Posts.create_post(conn.assigns[:user], post) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post created successfully!")
