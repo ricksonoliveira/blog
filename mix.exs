8a76ca004e5b8c8c1733815cffb341b61d59f75a
ricksonolveira <ricksonoliveira>
2022-02-07 17:42:31 -0300

    fix: refactoring tests

--- mix.exs
+++ mix.exs
@@ -92,7 +92,7 @@ defmodule Blog.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      test: ["ecto.reset --quiet", "test"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
